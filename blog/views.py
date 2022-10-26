f06cf75e130410f4eaba1ce008d7453c71dc7f89
DAJEONG59 <dajeongooo@ajou.ac.kr>
2022-10-26 16:12:00 +0900

    blog.index view에 post 목록 조회 추가

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # 장고의 템플릿 시스템
 
 def index(request):
-    return render(request,"blog/index.html")
+    #전체 포스팅을 가져올 준비 , 아직 가져오지는 않음.
+    post_qs = Post.objects.all().order_by("-id") #id필드에 대한 역순 정렬 (최신순)
+    return render(request,"blog/index.html",{
+        "post_list": post_qs
+    })
+
