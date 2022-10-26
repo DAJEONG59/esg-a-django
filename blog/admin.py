d4427b6ff700c384c1d2f5099b362e8527c4b3d6
DAJEONG59 <dajeongooo@ajou.ac.kr>
2022-10-26 12:20:31 +0900

    add post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from blog.models import Post
 # Register your models here.
+
+admin.site.register(Post) # 모델을 admin에 등록
+
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+from email import contentmanager
+from turtle import title
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
 # Create your models here.
+
+# post model
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+    created_at = models.DateTimeField()
+
+
